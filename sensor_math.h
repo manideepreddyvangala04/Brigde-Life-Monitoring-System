eae3bf9972787bad651edae5f3d2b71e416c31ac
Manideep Reddy Vangala <reddymanideep637@gmail.com>
2026-06-27 10:21:13 +0530

    Create sensor_math.h

---
+++ sensor_math.h
@@ -0,0 +1,22 @@
+#ifndef SENSOR_MATH_H
+#define SENSOR_MATH_H
+
+#include <math.h>
+#include <Adafruit_Sensor.h>
+
+// Applies the Complementary Filter to calculate stable structural pitch and roll
+void updateStructuralTilt(sensors_event_t a, sensors_event_t g, float dt, float &pitch, float &roll, float &z_gforce) {
+    
+    // 1. Calculate Pitch and Roll from Accelerometer
+    float accelPitch = atan2(a.acceleration.y, sqrt(a.acceleration.x * a.acceleration.x + a.acceleration.z * a.acceleration.z)) * 180 / PI;
+    float accelRoll = atan2(-a.acceleration.x, a.acceleration.z) * 180 / PI;
+
+    // 2. Complementary Filter (Fuses Gyro and Accelerometer data)
+    pitch = 0.98 * (pitch + g.gyro.x * dt) + 0.02 * accelPitch;
+    roll  = 0.98 * (roll  + g.gyro.y * dt) + 0.02 * accelRoll;
+
+    // 3. Convert Z-axis acceleration to G-forces
+    z_gforce = abs(a.acceleration.z / 9.81);
+}
+
+#endif // SENSOR_MATH_H
